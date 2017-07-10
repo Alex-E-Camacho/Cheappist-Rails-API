64736bd72a7a197554920855b76e2121c43f031b
Alex-E-Camacho <alex.e.camacho@gmail.com>
2017-07-10 19:57:22 -0400

    Create #new and #create actions along with #user_params

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -10,4 +10,20 @@ class V1::UsersController < ApplicationController
 
     render json: @user, status: :ok
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+    render json: @user, status: :created
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
